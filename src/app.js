d488dd651022c411dddad858b8d4b994d87e3600
ManuDev03 <manuprajan03@gmail.com>
2021-09-09 18:35:14 +0530

    adding MongoDB connection

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const path = require('path')
 const dotenv = require('dotenv')
 const morganLogger = require('./middleware/logger')
 const userRouter = require('./routers/user.Router')
+require('./db/mongoose')
 
 //configuration
 dotenv.config({path:path.resolve(process.cwd(),"src/config/.env")})
